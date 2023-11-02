f2d1e97cb5b688b552d0d459a414787c2f0ff84d
AditiShelke <aditishelke333@gmail.com>
2023-11-03 04:04:25 +0530

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include JS/JSX/TS/TSX files in this directory
+    './public/index.html', // Include your HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
